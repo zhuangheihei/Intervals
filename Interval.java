73e17d32feef532c7d1da8aac1dc8e68cbd5b628
Hong Zhuang <zhuanghong2013@gmail.com>
2018-02-11 01:17:22 +0000

    Interval class

---
+++ Interval.java
@@ -0,0 +1,12 @@
+public class Interval {
+    int start;
+    int end;
+    public Interval(){
+        this.start = 0;
+        this.end = 0;
+    }
+    public Interval(int s, int e){
+        this.start = s;
+        this.end = e;
+    }
+}
